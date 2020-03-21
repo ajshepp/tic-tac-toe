84794083d64f1d3432f46373a1a3fd587878702c
ajshepp <austinjsheppard@gmail.com>
2020-03-21 14:34:58 -0600

    I added is valid and while loop for turn management

--- main.py
+++ main.py
@@ -9,23 +9,39 @@ def create_board():
 
 # This function prints a board
 def print_board(board): 
-    print("=====")
+    print()
+    print("----")
+    print()
     for row in board:
         print(" ".join(row))
 
-# def place_x(board, row, col):
-#     board[row][col] = "X"
-# def place_o(board, row, col):
-#     board[row][col] = "O"
-def place_mark(board, row, col, mark):
-    board[row][col] = mark
+def is_valid(board, row, col):
+    if board[row][col] == "?":
+        return True
+    else:
+        print()
+        print("Invalid entry")
+        print()
+        return False
 
 
+def place_mark(board, row, col, player):
+    board[row][col] = player
+
+    
 board1 = create_board()
-print_board(board1)
-place_mark(board1, 1, 2, "X")
-place_mark(board1, 0, 2, "O")
-print_board(board1)
+player = "X"
 
+while True:
+    row = int(input("P" + player + " set row: "))
+    col = int(input("P" + player + " set column: "))
 
+    if is_valid(board1, row, col) == True:
+        place_mark(board1, row, col, player)
+        print_board(board1)
 
+        # Player switch
+        if player == "X":
+            player = "O"
+        elif player == "O":
+            player = "X"
